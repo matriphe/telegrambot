3e763a4526a53e5be5a2768d81af3c95dd8ff25f
Muhammad Zamroni <halo@matriphe.com>
2015-06-28 05:11:59 +0700

    First commit.

---
+++ src/Telegrambot.php
@@ -0,0 +1,71 @@
+<?php
+namespace Matriphe\Telegrambot;
+
+use GuzzleHttp\Client;
+
+class Telegrambot
+{
+    /**
+     * Telegram bot's API Token
+     * @var string
+     */
+    protected $apikey;
+    
+    /**
+     * Guzzle Client
+     * @var Client
+     */
+    protected $client;
+    
+    /**
+     * Constructor
+     * @param $apikey
+     */
+    public function __construct($apikey)
+    {
+        $this->apikey = $apikey;
+        $this->url = 'https://api.telegram.org/bot'.$this->apikey.'/';
+        
+        $this->client = new \GuzzleHttp\Client();
+    }
+    
+    /**
+     * Call method
+     * @param string $method
+     * @param array $data
+     * @return mixed
+     */
+    public function call($method, $data = null)
+    {
+        $request = $this->client->createRequest(
+            'post',
+            $this->url.$method,
+            [
+                'headers' => [
+                    //'Content-Type' => 'multipart/form-data',
+                ],
+                'body' => $data,
+            ]
+        );
+        $response = $this->client->send($request);
+        
+        $results = $response->json();
+        
+        if (!$results['ok']) {
+            throw new Exception($results['description'], $results['error_code']);
+        }
+        
+        return $results['result'];
+    }
+    
+    /**
+     * Use method as function
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return $this->call($name, $arguments ? $arguments[0] : null);
+    }
+}
